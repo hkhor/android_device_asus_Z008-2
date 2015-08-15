577325f49ed0bc0505db9883c300ffb08970674f
Nicholas Pace <nicholaspace97@yahoo.ca>
2015-08-14 21:56:08 -0400

    Update and rename cm.mk to full_Z00A.mk

--- full_Z00A.mk
+++ full_Z00A.mk
@@ -5,7 +5,6 @@ TARGET_SCREEN_WIDTH := 1280
 # Inherit some common CM stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z008/device.mk)
@@ -15,11 +14,11 @@ DEVICE_PACKAGE_OVERLAYS += device/asus/Z008/overlay
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_Z008
+PRODUCT_NAME := pa_Z00A
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := ASUS_Z008
+PRODUCT_MODEL := ASUS_Z00A
 PRODUCT_MANUFACTURER := asus
-PRODUCT_DEVICE := Z008
+PRODUCT_DEVICE := Z00A
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=WW_Z008 \

c698265348e1f480d9b43ac1302ed34aed3b28e2
Nicholas Pace <nicholaspace97@yahoo.ca>
2015-08-14 21:58:15 -0400

    Update and rename full_Z00A.mk to full_Z008.mk

--- full_Z008.mk
+++ full_Z008.mk
@@ -14,9 +14,9 @@ DEVICE_PACKAGE_OVERLAYS += device/asus/Z008/overlay
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := pa_Z00A
+PRODUCT_NAME := pa_Z008
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := ASUS_Z00A
+PRODUCT_MODEL := ASUS_Z008
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00A
 
